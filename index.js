664125d0018d9cf8e6e29827d36d1311c29f924d
Siarhei Lunski <loonskai@gmail.com>
2021-01-04 13:39:37 +0300

    Setup webhook

--- index.js
+++ index.js
@@ -6,19 +6,51 @@ const os = require('os');
 const path = require('path');
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.TOKEN, {
-  polling: true,
+const {
+  HOST,
+  PORT,
+  TOKEN,
+} = process.env;
+
+const bot = new TelegramBot(TOKEN, {
+  webHook: {
+    port: Number(PORT),
+  },
 });
 
-bot.onText(/\/echo (.+)/, (msg, metadata) => {
-  const { id: chatId } = msg.chat;
-  const resp = metadata[1];
+bot.setWebHook(`${HOST}/bot${TOKEN}`);
 
-  bot.sendMessage(chatId, resp);
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Welcome', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Configure',
+            switch_inline_query: 'Configure me',
+          },
+        ],
+      ],
+    },
+  });
+});
+
+bot.onText(/\/hi/, (msg) => {
+  console.log('Hello');
+  bot.sendMessage(msg.chat.id, 'Hello');
+});
+
+bot.onText(/\/info/, (msg) => {
+  const { id: chatId } = msg.chat;
+  console.log('HELLO');
+
+  bot.sendMessage(chatId, 'Info');
 });
 
 bot.on('message', (msg) => {
   const { id: chatId } = msg.chat;
+  const text = msg.text.toString().toLowerCase();
+  console.log(text);
 });
 
 bot.on('document', (msg) => {
